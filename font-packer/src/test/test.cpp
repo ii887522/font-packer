363e2e545360d0dfcc9b5d3b80df13764575939e
Chi Min Yong <chimin.yong@g2g.com>
2021-05-15 17:05:11 +0800

    Initial commit

---
+++ font-packer/src/test/test.cpp
@@ -0,0 +1,21 @@
+// Copyright ii887522
+
+#ifdef TEST
+
+#define ALLOCATOR_IMPLEMENTATIONS
+#include <Any/allocator.h>
+
+namespace ii887522::fontPacker {
+
+static int main() {
+
+  return EXIT_SUCCESS;
+}
+
+}  // namespace ii887522::fontPacker
+
+int main() {
+  return ii887522::fontPacker::main();
+}
+
+#endif
