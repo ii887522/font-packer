63cd79e6f7c298f467d7bd607001e7a6cc3e8db5
Chi Min Yong <chimin.yong@g2g.com>
2021-05-24 16:00:47 +0800

    Can check whether all font sizes are Uint

---
+++ font-packer/src/main/Functions/cli_ext.h
@@ -0,0 +1,12 @@
+// Copyright ii887522
+
+#ifndef FONT_PACKER_SRC_MAIN_FUNCGTIONS_CLI_EXT_H_
+#define FONT_PACKER_SRC_MAIN_FUNCGTIONS_CLI_EXT_H_
+
+namespace ii887522::fontPacker {
+
+bool isFontSizesUint(int argc, char** argv);
+
+}  // namespace ii887522::fontPacker
+
+#endif
