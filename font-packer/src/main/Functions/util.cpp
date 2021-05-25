c33bab6f4fa12060337c1464751d36c793b4c048
Chi Min Yong <chimin.yong@g2g.com>
2021-05-25 18:44:33 +0800

    Output FontName.h

--- font-packer/src/main/Functions/util.cpp
+++ font-packer/src/main/Functions/util.cpp
@@ -2,11 +2,53 @@
 
 #include "util.h"  // NOLINT(build/include_subdir)
 #include <nitro/Functions/util.h>
+#include <nitro/Functions/string_ext.h>
+#include <nitro/Functions/fs_ext.h>
+#include <string>
+#include <filesystem>
+#include <vector>
 #include "../Struct/Glyph.h"
 
 using ii887522::nitro::swap;
+using ii887522::nitro::getFileName;
+using ii887522::nitro::toUpperCase;
+using ii887522::nitro::write;
+using std::string;
+using std::filesystem::directory_iterator;
+using std::vector;
 
 namespace ii887522::fontPacker {
+
+string getFontNameEnumFileContent(const string& inputDirPath) {
+  string content{
+R"(// Copyright ii887522
+
+#ifndef FONT_PACKER_SRC_MAIN_ANY_FONT_NAME_H_
+#define FONT_PACKER_SRC_MAIN_ANY_FONT_NAME_H_
+
+namespace ii887522::fontPacker {
+
+enum class FontName : unsigned int {
+)"
+  };
+  for (const auto& entry : directory_iterator{ inputDirPath }) {
+    auto fontName{ getFileName(entry.path().string()) };
+    toUpperCase(&fontName);
+    content += "\t_" + fontName + ",\n";
+  }
+  return content +
+R"(};
+
+}  // namespace ii887522::fontPacker
+
+#endif  // FONT_PACKER_SRC_MAIN_ANY_FONT_NAME_H_
+)";
+}
+
+void writeFontNameEnumFile(const string& inputDirPath, const string& outputDirPath) {
+  const auto content{ getFontNameEnumFileContent(inputDirPath) };
+  write<char, vector>(outputDirPath + "FontName.h", vector(content.begin(), content.end()));
+}
 
 void rotate(Glyph*const glyph) {
   swap(glyph->imageRect.size.w, glyph->imageRect.size.h);  // NOLINT(build/include_what_you_use)
--- font-packer/src/main/Functions/util.h
+++ font-packer/src/main/Functions/util.h
@@ -4,8 +4,18 @@
 #define FONT_PACKER_SRC_MAIN_FUNCTIONS_UTIL_H_
 
 #include "../Struct/Glyph.h"
+#include <string>
+
+using std::string;
 
 namespace ii887522::fontPacker {
+
+// Param inputDirPath: it must exists and ends with either '/' or '\\'
+string getFontNameEnumFileContent(const string& inputDirPath);
+
+// Param inputDirPath: it must exists and ends with either '/' or '\\'
+// Param outputDirPath: it must ends with either '/' or '\\'
+void writeFontNameEnumFile(const string& inputDirPath, const string& outputDirPath);
 
 // Param glyph: it must not be assigned to nullptr or integer
 // See alsp ../Struct/Glyph.h for more details
