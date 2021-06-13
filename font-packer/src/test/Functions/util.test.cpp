e5964b5249c93e28b72d2b0c6320a2385426d995
Chi Min Yong <chimin.yong@g2g.com>
2021-06-13 22:16:36 +0800

    Export structures

--- font-packer/src/test/Functions/util.test.cpp
+++ font-packer/src/test/Functions/util.test.cpp
@@ -14,13 +14,9 @@ namespace ii887522::fontPacker {
 
 TEST_CASE("test rotate() function") {
   {
-    Glyph glyph{ 0u, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, 1, false };
+    Glyph glyph{ 0u, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, 1, false };
     rotate(&glyph);
     REQUIRE(glyph.atlasI == 0u);
-    REQUIRE(glyph.imageRect.position.x == 0);
-    REQUIRE(glyph.imageRect.position.y == 0);
-    REQUIRE(glyph.imageRect.size.w == 1);
-    REQUIRE(glyph.imageRect.size.h == 1);
     REQUIRE(glyph.rect.position.x == 0);
     REQUIRE(glyph.rect.position.y == 0);
     REQUIRE(glyph.rect.size.w == 1);
@@ -29,13 +25,9 @@ TEST_CASE("test rotate() function") {
     REQUIRE(glyph.isRotated);
   }
   {
-    Glyph glyph{ 1u, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, 1, false };
+    Glyph glyph{ 1u, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, 1, false };
     rotate(&glyph);
     REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 0);
-    REQUIRE(glyph.imageRect.position.y == 0);
-    REQUIRE(glyph.imageRect.size.w == 1);
-    REQUIRE(glyph.imageRect.size.h == 1);
     REQUIRE(glyph.rect.position.x == 0);
     REQUIRE(glyph.rect.position.y == 0);
     REQUIRE(glyph.rect.size.w == 1);
@@ -44,88 +36,9 @@ TEST_CASE("test rotate() function") {
     REQUIRE(glyph.isRotated);
   }
   {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 0 }, Size{ 1, 1 } }, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, 1, false };
+    Glyph glyph{ 1u, Rect{ Point{ 1, 0 }, Size{ 1, 1 } }, 1, false };
     rotate(&glyph);
     REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 0);
-    REQUIRE(glyph.imageRect.size.w == 1);
-    REQUIRE(glyph.imageRect.size.h == 1);
-    REQUIRE(glyph.rect.position.x == 0);
-    REQUIRE(glyph.rect.position.y == 0);
-    REQUIRE(glyph.rect.size.w == 1);
-    REQUIRE(glyph.rect.size.h == 1);
-    REQUIRE(glyph.advance == 1);
-    REQUIRE(glyph.isRotated);
-  }
-  {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 1, 1 } }, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, 1, false };
-    rotate(&glyph);
-    REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 1);
-    REQUIRE(glyph.imageRect.size.h == 1);
-    REQUIRE(glyph.rect.position.x == 0);
-    REQUIRE(glyph.rect.position.y == 0);
-    REQUIRE(glyph.rect.size.w == 1);
-    REQUIRE(glyph.rect.size.h == 1);
-    REQUIRE(glyph.advance == 1);
-    REQUIRE(glyph.isRotated);
-  }
-  {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 1 } }, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, 1, false };
-    rotate(&glyph);
-    REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 1);
-    REQUIRE(glyph.imageRect.size.h == 2);
-    REQUIRE(glyph.rect.position.x == 0);
-    REQUIRE(glyph.rect.position.y == 0);
-    REQUIRE(glyph.rect.size.w == 1);
-    REQUIRE(glyph.rect.size.h == 1);
-    REQUIRE(glyph.advance == 1);
-    REQUIRE(glyph.isRotated);
-  }
-  {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 1, 2 } }, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, 1, false };
-    rotate(&glyph);
-    REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 2);
-    REQUIRE(glyph.imageRect.size.h == 1);
-    REQUIRE(glyph.rect.position.x == 0);
-    REQUIRE(glyph.rect.position.y == 0);
-    REQUIRE(glyph.rect.size.w == 1);
-    REQUIRE(glyph.rect.size.h == 1);
-    REQUIRE(glyph.advance == 1);
-    REQUIRE(glyph.isRotated);
-  }
-  {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, Rect{ Point{ 0, 0 }, Size{ 1, 1 } }, 1, false };
-    rotate(&glyph);
-    REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 2);
-    REQUIRE(glyph.imageRect.size.h == 2);
-    REQUIRE(glyph.rect.position.x == 0);
-    REQUIRE(glyph.rect.position.y == 0);
-    REQUIRE(glyph.rect.size.w == 1);
-    REQUIRE(glyph.rect.size.h == 1);
-    REQUIRE(glyph.advance == 1);
-    REQUIRE(glyph.isRotated);
-  }
-  {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, Rect{ Point{ 1, 0 }, Size{ 1, 1 } }, 1, false };
-    rotate(&glyph);
-    REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 2);
-    REQUIRE(glyph.imageRect.size.h == 2);
     REQUIRE(glyph.rect.position.x == 1);
     REQUIRE(glyph.rect.position.y == 0);
     REQUIRE(glyph.rect.size.w == 1);
@@ -134,13 +47,9 @@ TEST_CASE("test rotate() function") {
     REQUIRE(glyph.isRotated);
   }
   {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, Rect{ Point{ 1, 1 }, Size{ 1, 1 } }, 1, false };
+    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 1, 1 } }, 1, false };
     rotate(&glyph);
     REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 2);
-    REQUIRE(glyph.imageRect.size.h == 2);
     REQUIRE(glyph.rect.position.x == 1);
     REQUIRE(glyph.rect.position.y == 1);
     REQUIRE(glyph.rect.size.w == 1);
@@ -149,13 +58,20 @@ TEST_CASE("test rotate() function") {
     REQUIRE(glyph.isRotated);
   }
   {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, Rect{ Point{ 1, 1 }, Size{ 2, 1 } }, 1, false };
+    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 1 } }, 1, false };
     rotate(&glyph);
     REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 2);
-    REQUIRE(glyph.imageRect.size.h == 2);
+    REQUIRE(glyph.rect.position.x == 1);
+    REQUIRE(glyph.rect.position.y == 1);
+    REQUIRE(glyph.rect.size.w == 1);
+    REQUIRE(glyph.rect.size.h == 2);
+    REQUIRE(glyph.advance == 1);
+    REQUIRE(glyph.isRotated);
+  }
+  {
+    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 1, 2 } }, 1, false };
+    rotate(&glyph);
+    REQUIRE(glyph.atlasI == 1u);
     REQUIRE(glyph.rect.position.x == 1);
     REQUIRE(glyph.rect.position.y == 1);
     REQUIRE(glyph.rect.size.w == 2);
@@ -164,13 +80,9 @@ TEST_CASE("test rotate() function") {
     REQUIRE(glyph.isRotated);
   }
   {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, 1, false };
+    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, 1, false };
     rotate(&glyph);
     REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 2);
-    REQUIRE(glyph.imageRect.size.h == 2);
     REQUIRE(glyph.rect.position.x == 1);
     REQUIRE(glyph.rect.position.y == 1);
     REQUIRE(glyph.rect.size.w == 2);
@@ -179,13 +91,9 @@ TEST_CASE("test rotate() function") {
     REQUIRE(glyph.isRotated);
   }
   {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, 2, false };
+    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, 2, false };
     rotate(&glyph);
     REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 2);
-    REQUIRE(glyph.imageRect.size.h == 2);
     REQUIRE(glyph.rect.position.x == 1);
     REQUIRE(glyph.rect.position.y == 1);
     REQUIRE(glyph.rect.size.w == 2);
@@ -194,28 +102,9 @@ TEST_CASE("test rotate() function") {
     REQUIRE(glyph.isRotated);
   }
   {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, 2, false };
+    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, 2, true };
     rotate(&glyph);
     REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 2);
-    REQUIRE(glyph.imageRect.size.h == 2);
-    REQUIRE(glyph.rect.position.x == 1);
-    REQUIRE(glyph.rect.position.y == 1);
-    REQUIRE(glyph.rect.size.w == 2);
-    REQUIRE(glyph.rect.size.h == 2);
-    REQUIRE(glyph.advance == 2);
-    REQUIRE(glyph.isRotated);
-  }
-  {
-    Glyph glyph{ 1u, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, Rect{ Point{ 1, 1 }, Size{ 2, 2 } }, 2, true };
-    rotate(&glyph);
-    REQUIRE(glyph.atlasI == 1u);
-    REQUIRE(glyph.imageRect.position.x == 1);
-    REQUIRE(glyph.imageRect.position.y == 1);
-    REQUIRE(glyph.imageRect.size.w == 2);
-    REQUIRE(glyph.imageRect.size.h == 2);
     REQUIRE(glyph.rect.position.x == 1);
     REQUIRE(glyph.rect.position.y == 1);
     REQUIRE(glyph.rect.size.w == 2);
